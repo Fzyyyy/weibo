ff78f1a89bc60b0784a1ba954948689b8ff6d913
Fzyyyy <605132784@qq.com>
2020-09-15 14:21:10 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>@yield('title','Weibo App')- Laravel 新手入门教程</title>
-	<link rel="stylesheet" href="{{mix('/css/app.css')}}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title','Weibo App')- Laravel 新手入门教程</title>
+  <link rel="stylesheet" href="{{mix('/css/app.css')}}">
 </head>
 <body>
 
-	@include('layouts._header')
-	<div class="container">
-		<div class="offset-md-1 col-md-10">
-		    @yield('content')
-		    @include('layouts._footer')
-		</div>
-    </div>
+@include('layouts._header')
+<div class="container">
+  <div class="offset-md-1 col-md-10">
+    @include('shared._messages')
+    @yield('content')
+    @include('layouts._footer')
+  </div>
+</div>
 
 </body>
 </html>
