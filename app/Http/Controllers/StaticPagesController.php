8914720b026057cfa4f7f2678648c4c65e8dd072
Fzyyyy <605132784@qq.com>
2020-09-16 11:52:36 +0000

    删除.envpro参数

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,15 +6,20 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    public function home(){
-    	return view('static_pages/home');
+    public function home()
+    {
+        $db_config = get_db_config();
+        var_dump($db_config);
+        return view('static_pages/home');
     }
 
-    public function help(){
-    	return view('static_pages/help');
+    public function help()
+    {
+        return view('static_pages/help');
     }
 
-    public function about(){
-    	return view('static_pages/about');
+    public function about()
+    {
+        return view('static_pages/about');
     }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -1,14 +1,15 @@
 <?php
+
 function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
         return $db_config = [
             'connection' => 'pgsql',
-            'host' => $url['host'],
-            'database' => substr($url['path'], 1),
-            'username' => $url["'user"],
-            'password' => $url['pass'],
+            'host' => $url["host"],
+            'database' => substr($url["path"], 1),
+            'username' => $url["user"],
+            'password' => $url["pass"],
         ];
     } else {
         return $db_config = [
